69d5c3de0c6c42db63a8eb1517b154f55974d079
unknown <dangkhoi2730@gmail.com>
2023-10-10 10:06:43 +0700

    use operator

---
+++ use-operator/use-operator.js
@@ -0,0 +1,8 @@
+var inputwidth;
+var inputheight;
+inputwidth=prompt("Nhập chiều rộng hình chữ nhật");
+inputheight=prompt("Nhập chiều cao hình chữ nhật");
+var width=parseInt(inputwidth);
+var height=parseInt(inputheight);
+var area=width*height;
+document.write("Diện tích hình chữ nhật là:"+area);
